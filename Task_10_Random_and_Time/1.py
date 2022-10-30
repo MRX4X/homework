5d9a1ab89d4381c929577cf2a4dfa1e99aa46dc4
denis <sdr72hoper@gmail.com>
2022-10-30 20:28:00 +0300

    time

--- Task_10_Random_and_Time/1.py
+++ Task_10_Random_and_Time/1.py
@@ -1,1 +1,12 @@
-'''В процессе'''
+from time import *
+start=time()
+while True:
+    a=int(input())
+    b=30
+    end=time()
+    total=end-start
+    c=b-total
+    print('Осталось минут', c)
+    if total==30 or a==0:
+        print('игра окочена')
+        break
