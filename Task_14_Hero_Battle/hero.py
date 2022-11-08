d4ce5186b8f364b17027a838da2a8c16c296ffb4
denis <sdr72hoper@gmail.com>
2022-11-09 00:12:12 +0300

    Game

--- Task_14_Hero_Battle/hero.py
+++ Task_14_Hero_Battle/hero.py
@@ -1,4 +1,5 @@
 from time import *
+from random import *
 class Game():
     def privet(self):
         return ('Привет в игре Игра престолов')
@@ -15,21 +16,47 @@ class Game():
             return ('Такой локации пока нет в игре')
     def Time(self):
         return ('Вы идете по карте....')
-        sleep(5)
+
 class Fight_1():
-    def money(self):
-        global mon
-        mon=100
     def razboi(self):
-        return ('Вы встретили перед собой разбойников\nОни требуют откуп в размере 50 монет, у вас есть', mon, 'монет, иначе вам придется сражаться\nВыбирайте: ОТКУП или СРАЖЕНИЕ')
+        global mon
+        mon = 100
+        print('Вы встретили перед собой разбойников\nОни требуют откуп в размере 50 монет, у вас есть', mon, 'монет, иначе вам придется сражаться\nВыбирайте: ОТКУП или СРАЖЕНИЕ')
         raz=input()
         if raz=='Откуп':
             raz_ploh=mon/2
-            return ('Вы откупились, но ваше количество монет стало', raz_ploh)
+            print('Вы откупились, но ваше количество монет стало', raz_ploh)
         if raz=='Сражение':
-            s='Вызвать файл с файтом'
+            print(
+                'Вы выбрали сражение. Из средств обороны у вас есть только кулаки.\nВыберите действие, указав число\n1)"удар"\n2)"хил"')
+            vi = 100
+            raz_1 = 100
+            print('У вас hp -', vi, 'У разбойника -', raz_1)
+            while True:
+                hit = int(input())
+                if hit == 1:
+                    b = randint(20, 60)
+                    raz_1 -= b
+                    print('У разбойника осталось', raz_1)
+                    c = randint(1, 4)
+                    vi -= c
+                    print('У вас осталось', vi)
+                if hit == 2:
+                    vi += randint(10, 30)
+                    print('Ваше хп', vi)
+                if vi <= 0:
+                    print('Вы проиграли')
+                    break
+                if raz_1 <= 0:
+                    print('Вы выйграли')
+                    break
 f1=Game()
 print(f1.privet())
 print(f1.Location())
 print(f1.Time())
+
+sleep(2)
+
+f2=Fight_1()
+print(f2.razboi())
 
