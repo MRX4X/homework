27fa213db2d31a77e4b04c5515b9a53b7d9fe42f
Sharapov Denis <sdr72hoper@gmail.com>
2022-12-22 12:30:17 +0300

    5 ЗАДАЧА

--- Zachet_5_zadacha.py
+++ Zachet_5_zadacha.py
@@ -1,10 +1,9 @@
 def reverseLookup(val):
     dict = {1: 3, 2: 5, 3:5}
+    mas=[]
     count=0
     for i in dict:
-        if i in dict.keys():
-            if dict[i] == val:
-                count+=1
-    print('В словаре с таким занчением', count, 'ключей')
-
+        if dict[i] == val:
+            mas.append(i)
+    print('С таким значением', *mas, 'ключи')
 reverseLookup(5)
