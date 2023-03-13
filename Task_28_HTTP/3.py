52bf3124708e77d4f2258be936af16ef7fc3a8e9
Sharapov Denis <sdr72hoper@gmail.com>
2023-03-13 22:36:49 +0300

    HTTP

--- Task_28_HTTP/3.py
+++ Task_28_HTTP/3.py
@@ -1,3 +1,3 @@
 import requests
-r=requests.get('https://rickandmortyapi.com/').text
-print(r)
+r=requests.get('https://rickandmortyapi.com/api/character').json()
+print('Имя', r['results'][0]['name'], 'планета', r['results'][0]['location']['name'])
