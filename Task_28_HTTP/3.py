0fd347264f40f0dba2a0e157a24f4e800fba2971
Sharapov Denis <sdr72hoper@gmail.com>
2023-03-15 22:14:55 +0300

    HTTP

--- Task_28_HTTP/3.py
+++ Task_28_HTTP/3.py
@@ -1,3 +1,8 @@
 import requests
 r=requests.get('https://rickandmortyapi.com/api/character').json()
+q=requests.get('https://rickandmortyapi.com/api/episode').json()
 print('Имя', r['results'][0]['name'], 'планета', r['results'][0]['location']['name'])
+ids=r['results'][0]['id']
+ids_last=r['results'][19]['id']
+for i in range(ids, ids_last):
+    print(q['results'][ids+1]['characters'])
