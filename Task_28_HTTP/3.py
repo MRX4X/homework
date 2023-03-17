e0290bc8e65db6eb90761ccd67bd1054014a963a
Sharapov Denis <sdr72hoper@gmail.com>
2023-03-17 14:46:29 +0300

    HTTP

--- Task_28_HTTP/3.py
+++ Task_28_HTTP/3.py
@@ -1,8 +1,21 @@
+# import requests
+# r=requests.get('https://rickandmortyapi.com/api/character').json()
+# q=requests.get('https://rickandmortyapi.com/api/episode').json()
+# print('Имя', r['results'][0]['name'], 'планета', r['results'][0]['location']['name'])
+# ids=r['results'][0]['id']
+# ids_last=r['results'][19]['id']
+# for i in range(ids, ids_last):
+#     print(q['results'][ids+1]['characters'])
+
 import requests
 r=requests.get('https://rickandmortyapi.com/api/character').json()
-q=requests.get('https://rickandmortyapi.com/api/episode').json()
-print('Имя', r['results'][0]['name'], 'планета', r['results'][0]['location']['name'])
+# print(r)
+# print('Имя', r['results'][0]['name'], 'планета', r['results'][0]['location']['name'])
 ids=r['results'][0]['id']
-ids_last=r['results'][19]['id']
+ids_last=r['results'][ids*5]['id']
+
 for i in range(ids, ids_last):
-    print(q['results'][ids+1]['characters'])
+    if ids==ids_last:
+        print('Имя', r['results'][ids_last]['name'], 'планета', r['results'][ids_last]['location']['name'], r['results'][ids_last]['episode'])
+    print('Имя', r['results'][ids]['name'], 'планета', r['results'][ids]['location']['name'], r['results'][ids]['episode'])
+    ids+=1
