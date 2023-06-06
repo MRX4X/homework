511d100b0d9f13dab80baab7f5d996f5ada3da68
Sharapov Denis <92737865+MRX4X@users.noreply.github.com>
2023-06-06 15:34:22 +0300

    Delete Zachet_5_zadacha.py

--- Zachet_5_zadacha.py
+++
@@ -1,9 +0,0 @@
-def reverseLookup(val):
-    dict = {1: 3, 2: 5, 3:5}
-    mas=[]
-    count=0
-    for i in dict:
-        if dict[i] == val:
-            mas.append(i)
-    print('С таким значением', *mas, 'ключи')
-reverseLookup(5)
